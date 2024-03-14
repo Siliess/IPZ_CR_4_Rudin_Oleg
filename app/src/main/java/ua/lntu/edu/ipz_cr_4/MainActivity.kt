241469ed208dc3e21b6020ba7792e92269a90351
Siliess <silies991@gmail.com>
2024-03-14 14:53:50 +0200

    Setting Task item element and write layout TaskListScreen

--- app/src/main/java/ua/lntu/edu/ipz_cr_4/MainActivity.kt
+++ app/src/main/java/ua/lntu/edu/ipz_cr_4/MainActivity.kt
@@ -15,6 +15,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
@@ -59,3 +63,48 @@ fun TaskManager() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskListScreen(navController: NavHostController) {
+    val tasks = remember { generateTaskList() }
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Список завдань") }) }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            tasks.forEach { task ->
+                TaskItem(task = task, onClick = {
+                    navController.navigate("taskDetail/${task.id}")
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskItem(task: Task, onClick: () -> Unit) {
+    Card(
+        colors = if (task.status == TaskStatus.ACTIVE) Color.Green else Color.Red,
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = task.name, style = MaterialTheme.typography.bodyMedium, color = Color.White)
+        }
+    }
+}
+
+data class Task(val id: String, val name: String, val description: String, val date: Date, val status: TaskStatus)
+
+enum class TaskStatus {
+    ACTIVE, DONE
+}
+
+fun generateTaskList(): List<Task> {
+    return listOf(
+        Task("1", "Завдання 1", "Опис завдання 1", Date(), TaskStatus.ACTIVE),
+        Task("2", "Завдання 2", "Опис завдання 2", Date(), TaskStatus.DONE),
+        Task("3", "Завдання 3", "Опис завдання 3", Date(), TaskStatus.ACTIVE)
+    )
+}
